c3475f15e229c1efc935facb693685d542639c8e
Bharath <31777542+bharathulaganathan@users.noreply.github.com>
2026-04-28 22:20:43 +0530

    Add: search-for-word-ii - submission-1

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-1.py
@@ -0,0 +1,39 @@
+class Trie:
+    def __init__(self):
+        self.child = dict()
+        self.index = -1
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        self.m = len(board)
+        self.n = len(board[0])
+        self.root = Trie()
+        for i, word in enumerate(words):
+            cur = self.root
+            for w in word:
+                if w not in cur.child:
+                    cur.child[w] = Trie()
+                cur = cur.child[w]
+            cur.index = i
+        self.res = list()
+        def checkwords(cur, x, y, done):
+            s = board[x][y]
+            if s in cur.child:
+                cur = cur.child[s]
+            else:
+                return
+            if cur.index >= 0:
+                self.res.append(words[cur.index])
+                cur.index = -1
+            done.add((x,y))
+            for i, j in [[0,1],[0,-1],[1,0],[-1,0]]:
+                nx = x+i
+                ny = y+j
+                if (nx,ny) not in done:
+                    new_done = done.copy()
+                    if nx >= 0 and nx < self.m and ny >= 0 and ny < self.n:
+                        checkwords(cur, nx, ny, new_done)
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                checkwords(self.root, i, j, set())
+        return self.res
