adffe035f804db0e402fe03976d6cae89f624232
Bharath <31777542+bharathulaganathan@users.noreply.github.com>
2026-05-05 22:08:39 +0530

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if len(prerequisites) == 0:
+            return True
+        def dfs(done,next):
+            if next in done:
+                return False
+            done = done.copy()
+            done.add(next)
+            res = True
+            for c in course[next]:
+                res = res and dfs(done,c)
+            return res
+        course = [[] for _ in range(numCourses)]
+        for done, req in prerequisites:
+            course[done].append(req)
+        for n in range(numCourses):
+            if not dfs(set(),n):
+                return False
+        return True
+
+        
