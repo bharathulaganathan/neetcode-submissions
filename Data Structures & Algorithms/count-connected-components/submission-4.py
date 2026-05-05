f9395df68ca9606b673aae2823751601a1569e54
Bharath <31777542+bharathulaganathan@users.noreply.github.com>
2026-05-05 23:00:34 +0530

    Add: count-connected-components - submission-4

---
+++ Data Structures & Algorithms/count-connected-components/submission-4.py
@@ -0,0 +1,31 @@
+class Union:
+    def __init__(self,n):
+        self.parent = [i for i in range(n)]
+        self.root = [0] * n
+    def find(self,x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    def join(self,x,y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x == root_y:
+            return
+        if self.root[root_x] > self.root[root_y]:
+            self.parent[root_y] =  x
+        elif self.root[root_y] > self.root[root_x]:
+            self.parent[root_x] =  y
+        else:
+            self.parent[root_y] = x
+            self.root[x] += 1
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        union = Union(n)
+        for x,y in edges:
+            union.join(x,y)
+        res = set()
+        for i in range(n):
+            union.find(i)
+            res.add(union.parent[i])
+        return len(res)
