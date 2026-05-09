24f2350a67238d58f126d6e2253477f6e31d1a3f
Bharath <31777542+bharathulaganathan@users.noreply.github.com>
2026-05-09 23:28:02 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 2:
+            return nums[0]
+        mem = dict()
+        def robs(x,s):
+            if x == n:
+                return 0
+            if x == n-1:
+                if s == 0:
+                    return 0
+                return nums[n-1]
+            if x == n-2:
+                return nums[n-2]
+            if x+2 not in mem:
+                mem[x+2] = robs(x+2,s)
+            skip = mem[x+2]
+            if x+3 not in mem:
+                mem[x+3] = robs(x+3,s)
+            jump = mem[x+3]
+            return nums[x] + max(skip, jump)
+        return max(robs(0,0),robs(1,1))
+        
